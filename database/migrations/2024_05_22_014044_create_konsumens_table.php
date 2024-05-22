cc1930abfaa2eccb5e2a51f5a52dc3f37bf466cb
c-ristna <ctina056@gmail.com>
2024-05-22 08:51:18 +0700

    buat data konsumen

---
+++ database/migrations/2024_05_22_014044_create_konsumens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKonsumenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('konsumen', function (Blueprint $table) {
+            $table->id_konsumen();
+            $table->varchar('nama_konsumen', 20);
+            $table->text('alamat', 200);
+            $table->varchar('no_telepon', 20);
+            $table->date('terakhir_pembelian', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('konsumen');
+    }
+}
