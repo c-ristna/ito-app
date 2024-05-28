685a0bc2a235cbe34a773f8c0c6e0667854692ec
FebrianiNovianty <febriani.novianty@itbss.ac.id>
2024-05-28 11:10:17 +0700

    Buat Data Tabel Penjualan

--- app/Models/Penjualan.php
+++ app/Models/Penjualan.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Konsumen extends Model
+class Penjualan extends Model
 {
     use HasFactory;
 }
