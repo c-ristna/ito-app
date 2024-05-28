951c6dfa18b53b64ca2ce39736d4dc65b039404a
Richel Jayreh El Rapha S <124883884+Rchlsmsy@users.noreply.github.com>
2024-05-28 11:13:01 +0700

    buat data tabel keuangan

---
+++ database/migrations/2024_05_28_034404_create_keuangans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKeuangansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keuangans', function (Blueprint $table) {
+            $table->id(); 
+            $table->date('tanggal_keuangan');
+            $table->decimal('pemasukan', 20);
+            $table->decimal('pengeluaran', 20);
+            $table->decimal('saldo', 20);
+            $table->decimal('total', 20);
+            $table->timestamps();
+
+            $table->foreignId('penjualan_id')->constrained('penjualan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keuangans');
+    }
+}
