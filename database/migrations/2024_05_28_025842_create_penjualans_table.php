3dec5ed6ae8101b6db07107d3446e7b2f3ba3f71
FebrianiNovianty <febriani.novianty@itbss.ac.id>
2024-05-29 08:40:13 +0700

    tabel

---
+++ database/migrations/2024_05_28_025842_create_penjualans_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePenjualansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penjualans', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal_penjualan');
+            $table->json('list_produk');
+            $table->decimal('total_harga', 20);
+            $table->string('metode_pembayaran', 20);
+            $table->string('status_penjualan', 20);
+            $table->timestamps();
+
+            // Define foreign keys
+            $table->foreignId('produk_id')->constrained('produks');
+            $table->foreignId('konsumen_id')->constrained('konsumen');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penjualans');
+    }
+}
