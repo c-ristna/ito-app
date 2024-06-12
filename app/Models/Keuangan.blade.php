3b1251d01766f19e97625a1942d013a79520d365
Richel Jayreh El Rapha S <124883884+Rchlsmsy@users.noreply.github.com>
2024-06-12 07:57:09 +0700

    ubah Keuangan

---
+++ app/Models/Keuangan.blade.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Keuangan extends Model
+{
+    use HasFactory;
+    public $table = 'keuangans';
+    
+    const CREATED_AT ='created_at';
+    const UPDATED_AT = 'updated_at';
+
+    protected $auditTimestamps = true;
+
+    public $fillable = [
+        'id',
+        'tanggal_keuangan',
+        'pemasukan',
+        'pengeluaran',
+        'saldo',
+        'total',
+        'penjualan_id',
+    ];
+}
